9f2bcd535f7d3893ac8abca2722387cdadbc05e1
Gaubert <gaubert.vinicius@gmail.com>
2022-04-29 22:17:54 -0700

    Finish project 2
    - Add more modularization
    - Implement partition printing before threads are joined
    - Improve comments
    - Clean up code

--- project-2--multithreading/c_multi_threaded_merge_sort.cpp
+++ project-2--multithreading/c_multi_threaded_merge_sort.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <cctype>
 #include <string>
-#include <cstdlib>
 #include <vector>
 #include <algorithm>
 #include <utility>
 #include <cmath>
-#include <atomic>
 #include <pthread.h>
 
-#define LOWER_BOUND 0
+#define LOWER_BOUND 1
 #define USAGE_ERROR 1
 
+// Struct type to hold arguments for sorting operations
+typedef struct {
+	std::pair<size_t, size_t> idx_pair;
+	size_t i;
+} sorting_op_args_t;
+
+// Struct type to hold arguments for merging operations
 typedef struct {
 	std::vector<int> in_part_a;
 	std::vector<int> in_part_b;
@@ -19,7 +23,10 @@ typedef struct {
 	size_t i;
 } merging_op_args_t;
 
+// rand_int_list: generated list of random integers (original list)
 std::vector<int> rand_int_list;
+
+// pairs: tuple to hold both starting and ending indices of each partition
 std::vector<std::pair<size_t, size_t>> pairs;
 
 // Temporary vectors for merging operations 
@@ -29,33 +36,36 @@ std::vector<std::pair<size_t, size_t>> pairs;
 std::vector< std::vector<int> > input_partitions, 
 																output_partitions, 
 																pass_last_partitions;
+
 // last_partitions_merge_output: result of the merging of two pass_last_partitions
 std::vector<int> last_partitions_merge_output;
 
 void print_usage();
 void validate_argv(int &argc, char *argv[]);
-std::string dump_partition(std::vector<int> &partition);
+std::string dump_partition(std::vector<int> &partition); // overloaded
 void generate_list(const size_t &n, const size_t &upper_bound);
 void print_list(std::vector<int> &list);
 void set_partition_delimiters(const size_t &n, const size_t &p);
-std::string dump_partition(std::vector<int> &partition, std::pair<size_t, size_t> &pair);
-void print_partitions();
+std::string dump_partition(std::vector<int> &partition, std::pair<size_t, size_t> &pair); // overloaded
+void print_partitions(); // overloaded
+void display_sort_msg(std::pair<size_t, size_t> idx_pair, size_t part_id);
 void* cpp_sort(void *args_ptr);
+void sort_partitions_multithreaded(std::vector<pthread_t>& threads, const size_t& p);
+void print_partitions(std::vector< std::vector<int> >& partitions); // overloaded
 void display_merge_msg(std::vector<int>& in_part_a, std::vector<int>& in_part_b, std::vector<int>& out_part, size_t part_id);
 void* cpp_merge(void* args_ptr);
 void merge_partitions_multithreaded(std::vector<pthread_t>& threads, 
 																		const size_t& p);
-void print_partitions(std::vector< std::vector<int> >& partitions);
 
 int main(int argc, char *argv[]) {
 	// Validate arguments that are passed in upon running executable
 	validate_argv(argc, argv);
 
-	// n: Number of elements to be generated
+	// n: number of elements to be generated
 	const size_t n = std::stoi(argv[1]);
-	// upper_bound: Max. possible value of an integer element
+	// upper_bound: max. possible value of an integer element of the original list
 	const size_t upper_bound = std::stoi(argv[2]);
-	// p: Number of partitions to be created
+	// p: number of partitions to be created
 	const size_t p = std::stoi(argv[3]);
 
 	// Generate list of random integers
@@ -69,10 +79,10 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	// No need for partitioning or merging
+	// No need for partitioning and merging
 	if (p == 1) {
+		std::cout << "\nNo need for partitioning and merging. Result of sorting:\n\n  ";
 		std::sort(rand_int_list.begin(), rand_int_list.end());
-		std::cout << "\nNo need for partitioning or merging. Result of sorting:\n\n  ";
 		std::cout << dump_partition(rand_int_list) << std::endl;
 		return 0;
 	}
@@ -82,29 +92,18 @@ int main(int argc, char *argv[]) {
 	std::cout << "\nList breakdown into partitions:\n";
 	print_partitions();
 
-	// Sort partitions
+	// Sort partitions in a multithreaded and sorted way
+	std::cout << "\nPartitions after multithreaded sorting:\n\n";
 	std::vector<pthread_t> threads(p);
-	for (size_t i = 0; i < p; i++) {
-		pthread_create(&threads[i], NULL, &cpp_sort, (void *) &pairs[i]);	
-	}
-	// Synchronize all threads
-	for (size_t i = 0; i < p; i++) {
-		pthread_join(threads[i], NULL);
-	}
-	std::cout << "\nPartitions after multithreaded sorting:\n";
-	print_partitions();
+	sort_partitions_multithreaded(threads, p);
 
-	 // Merge partitions in a multithreaded and sorted way, building back single list
+	// Merge partitions in a multithreaded and sorted way, building back one single list
   std::cout << "\nMultithreaded merging of partitions:\n";
 	threads.clear();
-	// Make partitions handy
-	for (size_t i = 0; i < p; i++) 
-		input_partitions.push_back(std::vector<int> (rand_int_list.begin() + pairs[i].first,
-																								 rand_int_list.begin() + pairs[i].second));
   merge_partitions_multithreaded(threads, p);
 
-	std::cout << "\nResult of list partitioning, followed by multithreaded sorting, followed by multithreaded merging:\n\n  ";
-	rand_int_list = output_partitions[0]; // This assignment is part of the requirements
+	std::cout << "\nResult of list partitioning, followed by partition multithreaded sorting, followed by partition multithreaded merging:\n\n  ";
+	rand_int_list = output_partitions[0]; // This assignment is part of the requirements of the project
 	std::cout << dump_partition(rand_int_list) << std::endl;
 
 	return 0;
@@ -117,9 +116,9 @@ void print_usage() {
 						<< std::endl
 						<< "where" << std::endl
 						<< std::endl
-						<< "    <N> is an positive integer representing the size of your list of elements" << std::endl
-						<< "    <MAX_VALUE> is an positive integer representing the possible max. value of the list elements " << std::endl
-						<< "    <P> is an positive integer (greater than zero) representing the intended number of partitions to break down the list into" << std::endl
+						<< "    <N> is a positive integer representing the size of your list of elements" << std::endl
+						<< "    <MAX_VALUE> is a positive integer representing the possible max. value of the list elements " << std::endl
+						<< "    <P> is a positive integer (greater than zero) representing the intended number of partitions to break down the list into" << std::endl
 						<< std::endl
 						<< "Example:" << std::endl
 						<< "    $ ./multi_threaded_merge_sort 25 100 5" << std::endl
@@ -205,7 +204,7 @@ void print_list(std::vector<int> &list) {
 }
 
 void set_partition_delimiters(const size_t &n, const size_t &p) {
-	// pair: holds start and end indices of a given partition
+	// pair: holds starting and ending indices of a given partition
 	std::pair<size_t, size_t> pair;
 	for (size_t i = 0; i < p; i++) {
 		pair.first = i * floor(n / p);
@@ -232,20 +231,44 @@ std::string dump_partition(std::vector<int> &partition, std::pair<size_t, size_t
 
 void print_partitions() {
 	std::cout << std::endl;
-	for (int i = 0; i < pairs.size(); i++) {
+	for (int i = 0; i < pairs.size(); i++)
 		std::cout << "  Part. " << i << ": " << dump_partition(rand_int_list, pairs[i])
 							<< " (size=" << pairs[i].second - pairs[i].first << ")"
 							<< std::endl
 							<< std::flush;
-	}
+}
+
+void display_sort_msg(std::pair<size_t, size_t> idx_pair, size_t part_id) {
+  std::string str("  Part. " + std::to_string(part_id) + ": " + dump_partition(rand_int_list, idx_pair) 
+														 + " (size=" + std::to_string(idx_pair.second - idx_pair.first ) + ")\n");
+  std::cout << str;
 }
 
 void *cpp_sort(void *args_ptr) {
-	std::pair<size_t, size_t>* pair = static_cast<std::pair<size_t, size_t>*> (args_ptr);
-	// std::cout << ("(" + std::to_string(pair->first) + ", " + std::to_string(pair->second) + ") ");
-	std::sort(rand_int_list.begin() + pair->first,
-						rand_int_list.begin() + pair->second);
+	sorting_op_args_t* args = (sorting_op_args_t*) args_ptr;
+	std::sort(rand_int_list.begin() + args->idx_pair.first,
+						rand_int_list.begin() + args->idx_pair.second);
+	display_sort_msg(args->idx_pair, args->i);
 	pthread_exit(0);
+}
+
+void sort_partitions_multithreaded(std::vector<pthread_t>& threads, const size_t& p) {
+	std::vector<sorting_op_args_t> sorting_args;
+
+	// Populate sorting struct vector in preparation for sorting
+	for (size_t i = 0; i < p; i++)
+		sorting_args.push_back((sorting_op_args_t) {
+														.idx_pair = pairs[i],
+														.i = i
+													});
+
+	// Sort elements in each partition of the original random int list (same resource) in a multithreaded way
+	for (size_t i = 0; i < p; i++)	
+		pthread_create(&threads[i], NULL, &cpp_sort, (void *) &sorting_args[i]);	
+
+	// Synchronize all threads
+	for (size_t i = 0; i < p; i++)
+		pthread_join(threads[i], NULL);
 }
 
 void print_partitions(std::vector< std::vector<int> >& partitions) {
@@ -265,7 +288,10 @@ void display_merge_msg(std::vector<int>& in_part_a, std::vector<int>& in_part_b,
 
 void* cpp_merge(void* args_ptr) {
 	merging_op_args_t* args = (merging_op_args_t*) args_ptr;
+
+	// Output partition has the size of the sum of the two input partitions
 	args->out_part.resize(args->in_part_a.size() + args->in_part_b.size());
+
   std::merge(args->in_part_a.begin(), args->in_part_a.end(), 
 						 args->in_part_b.begin(), args->in_part_b.end(), 
 						 args->out_part.begin());
@@ -282,6 +308,13 @@ void merge_partitions_multithreaded(std::vector<pthread_t>& threads,
   // counter: pass counter
   size_t counter = 0;
 
+	// Prepare for multithreaded merging by creating temporary vectors 
+	for (size_t i = 0; i < p; i++) 
+		input_partitions.push_back(std::vector<int> (rand_int_list.begin() + pairs[i].first,
+																								 rand_int_list.begin() + pairs[i].second));
+
+	// pass_last_partition_m_args: holds the arguments for the last partition (Part. <p_before_merge-1>)
+	//														 at a given pass while merging partitions
 	merging_op_args_t pass_last_partition_m_args;
 
   while (p_after_merge > 0) {
@@ -292,19 +325,18 @@ void merge_partitions_multithreaded(std::vector<pthread_t>& threads,
 
 		std::vector<merging_op_args_t> merging_args;
 
-		for (size_t i = 0; i < p_after_merge; i++) {
+		// Populate merting struct vector in preparation for merging
+		for (size_t i = 0; i < p_after_merge; i++) 
 			merging_args.push_back((merging_op_args_t) {
 															.in_part_a = input_partitions[2*i],
 															.in_part_b = input_partitions[2*i + 1],
 															.i = i
 														});
-		}
 
     // Do multithreaded merging operations
 		threads.resize(p_after_merge);
-		for (size_t i = 0; i < p_after_merge; i++) {
+		for (size_t i = 0; i < p_after_merge; i++)
 			pthread_create(&threads[i], NULL, &cpp_merge, (void *) &merging_args[i]);	
-    }
 
     // Is the number of partitions before the merging operations odd?
     if (p_before_merge%2) {
@@ -319,7 +351,6 @@ void merge_partitions_multithreaded(std::vector<pthread_t>& threads,
 				pass_last_partition_m_args.i = p_after_merge;
 				threads.push_back(0);
 				pthread_create(&threads[threads.size() - 1], NULL, &cpp_merge, (void *) &pass_last_partition_m_args);
-        // threads.push_back(std::thread(cpp_merge, std::ref(pass_last_partitions[0]), std::ref(pass_last_partitions[1]), std::ref(last_partitions_merge_output)));
       }
     }
 
@@ -363,7 +394,6 @@ void merge_partitions_multithreaded(std::vector<pthread_t>& threads,
 																							.out_part = output_partitions[0],
 																							.i = 0
 																					 };
-    // threads.push_back(std::thread(cpp_merge, std::ref(input_partitions[0]), std::ref(pass_last_partitions[0]), std::ref(output_partitions[0])));
     pthread_create(&threads[0], NULL, &cpp_merge, (void *) &final_merging_args);
 		pthread_join(threads[0], NULL);
 		output_partitions.push_back(final_merging_args.out_part);
